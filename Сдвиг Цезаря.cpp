0357ab88a66d65abb393eaacc02d636d098ef95e
Татьяна <90142707+Kuzy-git@users.noreply.github.com>
2021-10-21 15:09:23 +0500

    Лаба5
    Обеспечить достаточный уровень качества ПО

--- Сдвиг Цезаря.cpp
+++ Сдвиг Цезаря.cpp
@@ -8,7 +8,7 @@
 using std::string;
 using std::vector;
 
-using dict = std::unordered_map<char, char>;
+using dict = std::unordered_map<char, char>; // Использование несортируемого словаря
 
 int main()
 {
@@ -16,29 +16,32 @@ int main()
     SetConsoleOutputCP(1251);
     string s;
     int k;
-    vector<char> result;
+    vector<char> result; // Создание вектора для вывода результата
     string alphabet("абвгдеёжзийклмнопрстуфхцчшщъыьэюя");
-    dict alphabet_map;
+    dict alphabet_map; // Перевод строки алфавита в словарь
     std:: cout << "Введите сообщение на русском языке строчными буквами, которое будет закодировано: " << std::endl;
-    std::getline(std::cin, s);
+    std::getline(std::cin, s); // Ввод текста
     std:: cout << "\nВведите сдвиг:" << std::endl;
-    std::cin >> k;
-    int n = alphabet.size();
-    k = ((k % n) + n) % n;
-    for (int i = 0; i < n; i++) {
-        auto ind = (i + k) % n;
-        alphabet_map[alphabet[i]] = alphabet[ind];
+    std::cin >> k; // Ввод сдвига
+    int n = alphabet.size(); // Подсчет количества символов в алфавите
+    k = ((k % n) + n) % n; // Условие, для шага больше чем количество символов в алфавите и для декодировки (отрицательного шага)
+    for (int i = 0; i < n; i++) // Цикл проходящий по каждому введенному символу
+    {
+        auto ind = (i + k) % n; // Считает индекс символа со сдвигом
+        alphabet_map[alphabet[i]] = alphabet[ind]; // Присваивание символу значения со здвигом
     }
     std::cout << "\nПолученное закодированное сообщение:" << std::endl;
     for (auto it : s) {
         try {
-            result.push_back(alphabet_map.at(it));
+            result.push_back(alphabet_map.at(it)); // Вставка изменённого символа в конечный результат 
         }
-        catch (const std::out_of_range& exception) {
+        catch (const std::out_of_range& exception) // Обработка исчключений для пробела и других символов. Возвращает то же, что и ввели
+        {
             result.push_back(it);
         }
     }
-    for (auto it : result) {
+    for (auto it : result) // Вывод результата
+    {
         std::cout << it;
     }
     _getch();
